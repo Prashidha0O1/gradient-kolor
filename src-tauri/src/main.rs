41676582b3c575cc594a8f9524a1c2c0a472358f
Prashidha0O1 <89299485+Prashidha0O1@users.noreply.github.com>
2024-02-07 15:51:30 +0545

    files required for gradd-kolor

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,16 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![generate_grad])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn generate_grad(r: u8, g: u8, b: u8, a: u8) {
+  println!("{} {} {} {}", r,g,b,a);
 }
--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,19 @@ import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import { SketchPicker } from "react-color";
+import { event } from "@tauri-apps/api";
+import { invoke } from "@tauri-apps/api/tauri";
 
 function App() {
+  const [color, setColor] = useState("#000000");
+  const handleChange = (newColor) => {
+    setColor(newColor.hex);
+  }
   return (
-   <SketchPicker />
+   <SketchPicker 
+   color={color}
+   onChange = {handleChange}
+   />
   );
 }
 
